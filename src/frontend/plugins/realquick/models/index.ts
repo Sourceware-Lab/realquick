9c8aff0339d8254679a89f1ec0d22013ea9689ee
Peter <peterHoburg@users.noreply.github.com>
2025-01-02 15:23:48 -0500

    Added codeowners file

--- src/frontend/plugins/realquick/models/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './error-detail';
-export * from './error-model';
-export * from './timeblock-get-output-body';
-export * from './timeblock-post-body-input';
-export * from './timeblock-post-output-body';
